351bd4b951e6f86d322ddb5636bfa86814514bf0
Loredana Cobzaru <cobzaru.loredana00@gmail.com>
2021-08-09 20:15:57 +0300

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Test for GitHub");
+        System.out.println("Another test because I'm dumb");
     }
 }
